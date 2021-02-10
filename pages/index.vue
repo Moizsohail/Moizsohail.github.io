c64f43b590b8fe81a77aeece9acac99d3685168b
Moiz Sohail <moizsohail@live.co.uk>
2021-02-10 13:28:11 +0500

    expansion services for mobile

--- pages/index.vue
+++ pages/index.vue
@@ -117,41 +117,7 @@
         </div>
       </section>
       <section class="capabilities" v-rellax="{ speed: 0 }">
-        <div>
-          <h2 class="text-overline">Services</h2>
-          <h2 class="text-h3">My capabilities</h2>
-        </div>
-        <div class="services row">
-          <div class="side col-lg-5 col-md-4 col-sm-12 col-12">
-            <ul>
-              <li
-                v-for="x in catskills"
-                :key="x"
-                :class="{ active: curr === x }"
-                @click="curr = x"
-              >
-                <p>
-                  {{ x }}
-                </p>
-              </li>
-            </ul>
-          </div>
-          <div class="col-lg-7 col-md-8 col-sm-12 col-12">
-            <div class="row">
-              <div
-                v-for="x in currentskills"
-                :key="x.icon"
-                class="col-md-6 col-sm-6 col-12 skill"
-              >
-                <div class="icon">
-                  <i :class="x.icon" aria-hidden="true">{{ x.it }} </i>
-                </div>
-                <h4>{{ x.title }}</h4>
-                <p>{{ x.desc }}</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Services />
       </section>
       <section v-rellax="{ speed: 0 }" class="contact">
         <h1 class="text-h1">Let's Make Something Great Together</h1>
@@ -163,10 +129,12 @@
 </template>
 <script>
 import SocialBar from '~/components/socialbar'
+import Services from '~/components/Services'
 
 export default {
   components: {
     SocialBar,
+    Services,
   },
   head: {
     title: 'Moiz Sohail',
@@ -192,92 +160,6 @@ export default {
   },
   data() {
     return {
-      curr: 'UI/UX Design',
-      skills: [
-        {
-          head: 'UI/UX Design',
-          title: 'Flutter Development',
-          icon: 'fab fa-android',
-          desc: '2 Projects',
-        },
-        {
-          head: 'UI/UX Design',
-          title: 'Nuxt/Vue Development',
-          icon: 'fab fa-vuejs',
-          desc: '3 Projects',
-        },
-        {
-          head: 'UI/UX Design',
-          title: 'HTML/CSS Development',
-          icon: 'fab fa-html5',
-          desc: '3 Projects',
-        },
-        {
-          head: 'UI/UX Design',
-          title: 'Figma/Balsamiq Prototyping',
-          icon: 'fab fa-figma',
-          desc: '3 Projects',
-        },
-
-        {
-          head: 'Backend Development',
-          title: 'Node JS',
-          icon: 'fab fa-node-js',
-          desc: '1 Project',
-        },
-        {
-          head: 'Backend Development',
-          title: 'SQL/Mongoose',
-          icon: 'fa fa-database',
-          desc: 'Database Managment',
-        },
-        {
-          head: 'Backend Development',
-          title: 'Digital Ocean Deployment',
-          icon: 'fab fa-digital-ocean',
-        },
-        {
-          head: 'Automation',
-          title: 'Selenium',
-          icon: 'fas fa-spider',
-        },
-        {
-          head: 'Automation',
-          title: 'Pyautogui',
-          icon: 'fas fa-robot',
-        },
-        {
-          head: 'Deep Learning',
-          title: 'TensorFlow',
-          icon: 'fas fa-brain',
-        },
-        {
-          head: 'Deep Learning',
-          title: 'Keras',
-          icon: 'fab fa-kickstarter-k',
-        },
-        {
-          head: 'Languages',
-          title: 'C++',
-          icon: 'fab fa-cuttlefish',
-          it: '++',
-        },
-        {
-          head: 'Languages',
-          title: 'Python',
-          icon: 'fab fa-python',
-        },
-        {
-          head: 'Languages',
-          title: 'Javascript',
-          icon: 'fab fa-js',
-        },
-        {
-          head: 'Languages',
-          title: 'Dart',
-          icon: 'fab fa-dyalog',
-        },
-      ],
       rellax: {
         speed: -0.5,
       },
@@ -361,14 +243,6 @@ export default {
         return ['']
       }
     },
-    currentskills() {
-      return this.skills.filter((x) => x.head === this.curr)
-    },
-    catskills() {
-      const cat = new Set()
-      this.skills.forEach((x) => cat.add(x.head))
-      return cat
-    },
   },
 }
 </script>
@@ -410,6 +284,7 @@ export default {
 .about-image {
   width: 300px;
 }
+
 .project {
   padding: 200px 0px 0px;
 }
@@ -439,6 +314,21 @@ export default {
   min-height: 98vh;
   /* padding: 380px 100px 380px 100px; */
 }
+.capabilities {
+  background: #1a1a1a;
+  color: rgba(255, 255, 255, 0.596);
+  padding: 100px 100px 150px 100px;
+  margin-top: -200px;
+}
+.capabilities .bgtext {
+  /* position: absolute; */
+  color: transparent;
+  font-weight: normal;
+  font-family: 'futuraheavy';
+  letter-spacing: 3px;
+  -webkit-text-stroke: 1px rgba(255, 255, 255, 0.2);
+  line-height: 220px;
+}
 .contact h1 {
   color: #f6f7fb;
   font-size: 50px !important;
@@ -457,62 +347,14 @@ export default {
   font-family: 'futurabook' !important;
 }
 
-.services .side ul {
-  margin-left: -10px;
-  padding: 0px;
-}
-.services .skill {
-  height: 200px;
-}
-.services .skill i {
-  font-size: 40px;
-  color: #595959;
-  line-height: 28px;
-}
-.services .skill p {
-  color: #c1c1c1;
-  font-weight: 300;
-  font-size: 15px;
-  font-family: 'Poppins', sans-serif;
-  line-height: 28px;
-}
 .cvtext {
   color: white;
-}
-.services .skill h4 {
-  color: #c1c1c1;
-  font-size: 18px;
-  cursor: pointer;
-  transition: 0.4s ease;
-  font-weight: 700;
-  line-height: 40px;
-  font-family: 'Poppins', sans-serif;
-  -webkit-transition: all 0.5s;
-  -o-transition: all 0.5s;
-  transition: all 0.5s;
-}
-.services .side li {
-  border-color: #dee2e6 #dee2e6 #fff;
-  font-size: 1.5em;
-  font-weight: 600;
-  line-height: 60px;
-  font-family: 'futuraheavy';
-  border: none;
-  padding: 0;
-  cursor: pointer;
-  transition: 0.4s ease;
-  display: block;
 }
 i {
   transition: 0.4s ease;
   cursor: pointer;
 }
-.services .skill:hover h4,
-.services .skill:hover i,
-.services .side li:hover,
-.services .side li.active {
-  color: #ff3366 !important;
-}
+
 .contact h3 {
   font-weight: 400;
   font-family: 'futurabook' !important;
@@ -552,27 +394,7 @@ i {
 .portfolio {
   padding: 50px 100px 0px 100px;
 }
-.capabilities {
-  background: #1a1a1a;
-  color: rgba(255, 255, 255, 0.596);
-  padding: 100px 100px 150px 100px;
-  margin-top: -200px;
-}
-.capabilities .bgtext {
-  /* position: absolute; */
-  color: transparent;
-  font-weight: normal;
-  font-family: 'futuraheavy';
-  letter-spacing: 3px;
-  -webkit-text-stroke: 1px rgba(255, 255, 255, 0.2);
-  line-height: 220px;
-}
-.capabilities .text-h3 {
-  font-size: 4em !important;
-  margin-top: 5px;
-  font-family: 'futuraheavy' !important;
-  margin-bottom: 40px;
-}
+
 .fixed {
   position: fixed;
 }
@@ -636,19 +458,17 @@ i {
   .socialbar {
     bottom: 7vh;
   }
+  .capabilities {
+    margin-top: 0px;
+    padding: 100px 20px 150px 20px;
+  }
   .about {
     padding: 200px 20px 50px 20px;
   }
   .portfolio {
     padding: 50px 20px 0px 20px;
   }
-  .capabilities {
-    margin-top: 0px;
-    padding: 100px 20px 150px 20px;
-  }
-  .capabilities .text-h3 {
-    font-size: 3em !important;
-  }
+
   .contact h1 {
     font-size: 40px !important;
   }
