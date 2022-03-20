35bfbbbd2c24fecc4b3ad4f58df214a61eafe79f
moiz-sohail <moiz.sohail@educative.io>
2022-03-20 17:41:04 +0500

    deploy

--- docs/_nuxt/static/1647778440/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fhistory","\u002Fportfolio\u002Fkebutterus","\u002Fportfolio\u002Fdementiacare","\u002Fportfolio\u002Fvlancer","\u002Fportfolio\u002Feducative","\u002Fportfolio\u002Fpsifixi","\u002Fportfolio\u002Fsurveybot","\u002Fportfolio","\u002F"]})
